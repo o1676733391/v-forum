dff1d60be2c810b7fc043e24a31caae631ecd625
o1676733391 <vinhhiencute@gmail.com>
2023-12-13 17:16:54 +0700

    fix children

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -4,4 +4,9 @@ export const FIREBASE_ERRORS = {
 
   "Firebase: Error (auth/user-not-found).": "Invalid email or password",
   "Firebase: Error (auth/wrong-password).": "Invalid email or password",
+  // OPERATION_NOT_ALLOWED
+  "Firebase: Error (auth/operation-not-allowed).":
+    "Password sign-in is disabled for this project",
+  // INVALID_EMAIL
+  
 };
