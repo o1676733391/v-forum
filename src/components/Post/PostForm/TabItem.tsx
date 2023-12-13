422206a316471419c6fc2be59666bde7c69c32ec
o1676733391 <vinhhiencute@gmail.com>
2023-12-13 17:24:56 +0700

    fix TabItem conflict

--- src/components/Post/PostForm/TabItem.tsx
+++ src/components/Post/PostForm/TabItem.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Flex, Icon, Text } from "@chakra-ui/react";
-import { TabItem } from "./NewPostForm";
+import { TabItem as TabItemType } from "./NewPostForm";
 
 type TabItemProps = {
-  item: TabItem;
+  item: TabItemType;
   selected: boolean;
   setSelectedTab: (value: string) => void;
 };
